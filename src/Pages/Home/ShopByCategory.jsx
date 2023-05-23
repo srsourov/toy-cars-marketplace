23a35a4f017c57b4e39819afb698088e5edae31e
SR Sourov <sourov.web@gmail.com>
2023-05-23 17:32:35 +0600

    cars added

--- src/Pages/Home/ShopByCategory.jsx
+++ src/Pages/Home/ShopByCategory.jsx
@@ -17,7 +17,7 @@ const ShopByCategory = () => {
         fetch(`http://localhost:5000/cars/1`)
             .then(res => res.json())
             .then(data => setCars(data))
-    },[])
+    }, [])
 
 
     return (
@@ -28,9 +28,9 @@ const ShopByCategory = () => {
             <div className='text-center'>
                 <Tabs>
                     <TabList className="border-none mb-4">
-                        <Tab style={{ backgroundColor: "#570DF8", borderRadius: "0px", color: "#ffffff", margin: "0 10px", padding: "10px 20px" }}> Racing Cars</Tab>
-                        <Tab style={{ backgroundColor: "#570DF8", borderRadius: "0px", color: "#ffffff", margin: "0 10px", padding: "10px 20px" }}> Off-Road Vehicles</Tab>
-                        <Tab style={{ backgroundColor: "#570DF8", borderRadius: "0px", color: "#ffffff", margin: "0 10px", padding: "10px 20px" }}> Classic Cars</Tab>
+                        <Tab onClick={() => loadCars("1")} style={{ backgroundColor: "#570DF8", borderRadius: "0px", color: "#ffffff", margin: "0 10px", padding: "10px 20px" }}> Racing Cars</Tab>
+                        <Tab onClick={() => loadCars("4")} style={{ backgroundColor: "#570DF8", borderRadius: "0px", color: "#ffffff", margin: "0 10px", padding: "10px 20px" }}> Off-Road Vehicles</Tab>
+                        <Tab onClick={() => loadCars("7")} style={{ backgroundColor: "#570DF8", borderRadius: "0px", color: "#ffffff", margin: "0 10px", padding: "10px 20px" }}> Classic Cars</Tab>
                     </TabList>
 
                     <TabPanel>
@@ -52,6 +52,76 @@ const ShopByCategory = () => {
                                 </div>
                             </TabPanel>
                             <TabPanel>
+                                <div className="grid grid-cols-2 gap-4 w-9/12 mx-auto my-8">
+                                    {
+                                        cars.map(car => <CarCard
+                                            key={car._id}
+                                            car={car}
+                                        ></CarCard>)
+                                    }
+                                </div>
+                            </TabPanel>
+                            <TabPanel>
+                                <div className="grid grid-cols-2 gap-4 w-9/12 mx-auto my-8">
+                                    {
+                                        cars.map(car => <CarCard
+                                            key={car._id}
+                                            car={car}
+                                        ></CarCard>)
+                                    }
+                                </div>
+                            </TabPanel>
+                        </Tabs>
+                    </TabPanel>
+                    <TabPanel>
+                        <Tabs>
+                            <TabList>
+                                <Tab onClick={() => loadCars("4")} style={{ backgroundColor: "#7C3DFF", color: "#ffffff", margin: "0 10px", padding: "10px 20px" }}>Monster Trucks</Tab>
+                                <Tab onClick={() => loadCars("5")} style={{ backgroundColor: "#7C3DFF", color: "#ffffff", margin: "0 10px", padding: "10px 20px" }}>SUVs and Jeeps</Tab>
+                                <Tab onClick={() => loadCars("6")} style={{ backgroundColor: "#7C3DFF", color: "#ffffff", margin: "0 10px", padding: "10px 20px" }}>Buggy Cars</Tab>
+                            </TabList>
+
+                            <TabPanel>
+                                <div className="grid grid-cols-2 gap-4 w-9/12 mx-auto my-8">
+                                    {
+                                        cars.map(car => <CarCard
+                                            key={car._id}
+                                            car={car}
+                                        ></CarCard>)
+                                    }
+                                </div>
+                            </TabPanel>
+                            <TabPanel>
+                                <div className="grid grid-cols-2 gap-4 w-9/12 mx-auto my-8">
+                                    {
+                                        cars.map(car => <CarCard
+                                            key={car._id}
+                                            car={car}
+                                        ></CarCard>)
+                                    }
+                                </div>
+                            </TabPanel>
+                            <TabPanel>
+                                <div className="grid grid-cols-2 gap-4 w-9/12 mx-auto my-8">
+                                    {
+                                        cars.map(car => <CarCard
+                                            key={car._id}
+                                            car={car}
+                                        ></CarCard>)
+                                    }
+                                </div>
+                            </TabPanel>
+                        </Tabs>
+                    </TabPanel>
+                    <TabPanel>
+                        <Tabs>
+                            <TabList>
+                                <Tab onClick={() => loadCars("7")} style={{ backgroundColor: "#7C3DFF", color: "#ffffff", margin: "0 10px", padding: "10px 20px" }}>Vintage Cars</Tab>
+                                <Tab onClick={() => loadCars("8")} style={{ backgroundColor: "#7C3DFF", color: "#ffffff", margin: "0 10px", padding: "10px 20px" }}>Retro Muscle Cars</Tab>
+                                <Tab onClick={() => loadCars("9")} style={{ backgroundColor: "#7C3DFF", color: "#ffffff", margin: "0 10px", padding: "10px 20px" }}>Antique Cars</Tab>
+                            </TabList>
+
+                            <TabPanel>
                             <div className="grid grid-cols-2 gap-4 w-9/12 mx-auto my-8">
                                     {
                                         cars.map(car => <CarCard
@@ -71,43 +141,15 @@ const ShopByCategory = () => {
                                     }
                                 </div>
                             </TabPanel>
-                        </Tabs>
-                    </TabPanel>
-                    <TabPanel>
-                        <Tabs>
-                            <TabList>
-                                <Tab style={{ backgroundColor: "#7C3DFF", color: "#ffffff", margin: "0 10px", padding: "10px 20px" }}>Monster Trucks</Tab>
-                                <Tab style={{ backgroundColor: "#7C3DFF", color: "#ffffff", margin: "0 10px", padding: "10px 20px" }}>SUVs and Jeeps</Tab>
-                                <Tab style={{ backgroundColor: "#7C3DFF", color: "#ffffff", margin: "0 10px", padding: "10px 20px" }}>Buggy Cars</Tab>
-                            </TabList>
-
                             <TabPanel>
-                                <h2>Any content 1</h2>
-                            </TabPanel>
-                            <TabPanel>
-                                <h2>Any content 2</h2>
-                            </TabPanel>
-                            <TabPanel>
-                                <h2>Any content 3</h2>
-                            </TabPanel>
-                        </Tabs>
-                    </TabPanel>
-                    <TabPanel>
-                        <Tabs>
-                            <TabList>
-                                <Tab style={{ backgroundColor: "#7C3DFF", color: "#ffffff", margin: "0 10px", padding: "10px 20px" }}>Vintage Cars</Tab>
-                                <Tab style={{ backgroundColor: "#7C3DFF", color: "#ffffff", margin: "0 10px", padding: "10px 20px" }}>Retro Muscle Cars</Tab>
-                                <Tab style={{ backgroundColor: "#7C3DFF", color: "#ffffff", margin: "0 10px", padding: "10px 20px" }}>Antique Cars</Tab>
-                            </TabList>
-
-                            <TabPanel>
-                                <h2>Any content 1</h2>
-                            </TabPanel>
-                            <TabPanel>
-                                <h2>Any content 2</h2>
-                            </TabPanel>
-                            <TabPanel>
-                                <h2>Any content 3</h2>
+                            <div className="grid grid-cols-2 gap-4 w-9/12 mx-auto my-8">
+                                    {
+                                        cars.map(car => <CarCard
+                                            key={car._id}
+                                            car={car}
+                                        ></CarCard>)
+                                    }
+                                </div>
                             </TabPanel>
                         </Tabs>
                     </TabPanel>
