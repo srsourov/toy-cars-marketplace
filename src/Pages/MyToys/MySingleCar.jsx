35d916b92e37be8bfad7291ead65f0afe9a41c61
SR Sourov <sourov.web@gmail.com>
2023-05-23 21:53:23 +0600

    my toys updated

---
+++ src/Pages/MyToys/MySingleCar.jsx
@@ -0,0 +1,46 @@
+import { Link } from "react-router-dom";
+
+const MySingleCar = ({car, handleDelete }) => {
+    const {toyPic, toyName,sellerName, subcategory, price, rating, availableQuantity, _id } = car;
+    
+
+    return (
+        <tr>
+            <th>
+                <button onClick={() => handleDelete(_id)} className="btn btn-circle">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                </button>
+            </th>
+            <td>
+                <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                        <div className="rounded w-12 h-12">
+                            {toyPic && <img src={toyPic} alt="Avatar Tailwind CSS Component" />}
+                        </div>
+                    </div>
+                </div>
+            </td>
+            <td>
+                {toyName}
+            </td>
+            <td>
+                {sellerName}
+            </td>
+            <td>
+                {subcategory}
+            </td>
+            <td>
+                ${price}
+            </td>
+            <td>{rating}</td>
+            <td>{availableQuantity}</td>
+            <th>
+                
+                <Link to="/updatecar/${_id}" className="btn btn-primary btn-xs">Update</Link>
+                
+            </th>
+        </tr>
+    );
+};
+
+export default MySingleCar;
--- src/Pages/MyToys/MyToys.jsx
+++ src/Pages/MyToys/MyToys.jsx
@@ -1,5 +1,6 @@
-import React, { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../Providers/AuthProvider';
+import MySingleCar from './MySingleCar';
 
 const MyToys = () => {
     const { user } = useContext(AuthContext);
@@ -12,11 +13,58 @@ const MyToys = () => {
             .then(res => res.json())
             .then(data => {
                 setCarAdded(data)
+                console.log(data)
             })
-    }, []);
+    }, [url]);
+
+
+    const handleDelete = id => {
+        const proceed = confirm("Are you sure you want to delete?")
+        if (proceed) {
+            fetch(`http://localhost:5000/carAdded/${id}`, {
+                method: "DELETE"
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    if (data.deletedCount > 0) {
+                        alert("deleted successfully");
+                        // const remaining = bookings.filter(booking => booking._id !== id);
+                        // setCarAdded(remaining)
+                    }
+                })
+        }
+    }
     return (
         <div>
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Photo</th>
+                            <th>Name</th>
+                            <th>sellerName</th>
+                            <th>subcategory</th>
+                            <th>price</th>
+                            <th>rating</th>
+                            <th>availableQuantity</th>
+                            <th>Update</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            carAdded.map(car => <MySingleCar
+                                car={car}
+                                key={car._id}
+                                handleDelete={handleDelete} 
+                            ></MySingleCar>)
+                        }
+                    </tbody>
 
+                </table>
+            </div>
         </div>
     );
 };
